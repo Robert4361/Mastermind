e8edd01f0768d3a644393b7aaebd50b5b1bb9332
Robert4361 <robert4361@gmail.com>
2022-12-08 21:39:13 +0100

    Add code checking and hinting

---
+++ code.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# This class handles generating and verifying game code
+class Code
+  attr_reader :value
+
+  def initialize
+    @value = generate
+  end
+
+  def generate
+    code_array = []
+    4.times do
+      code_array.push(rand(1..6))
+    end
+    code_array
+  end
+
+  def code_correct?(code)
+    code = code.chars.map(&:to_i)
+    return true if code == @value
+
+    get_clues(code)
+  end
+
+  def get_clues(code) # rubocop:disable Layout/MethodLength
+    clues = [0, 0]
+    4.times do |i|
+      if code[i] == @value [i]
+        clues[0] += 1
+        code[i] = 0
+        next
+      elsif code.include?(@value[i])
+        clues[1] += 1
+        code[code.index(@value[i])] = 0
+      end
+    end
+    clues
+  end
+end
