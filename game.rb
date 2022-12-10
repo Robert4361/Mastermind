cee797cf5b1abda62eab82a38e0e25b3f38c0597
Robert4361 <robert4361@gmail.com>
2022-12-10 11:52:56 +0100

    Add game playing method

--- game.rb
+++ game.rb
@@ -9,6 +9,30 @@ class Game
   def initialize
     Rules.show
     @code = Code.new
+    play
+  end
+
+  def turn
+    puts 'Write your code'
+    player_guess = 0
+    loop do
+      player_guess = gets.chomp.chars.map(&:to_i)
+      break unless player_guess.include?(0)
+    end
+    @code.code_correct?(player_guess)
+  end
+
+  def play # rubocop:disable Metrics/MethodLength
+    12.times do
+      result = turn
+      if result == true
+        puts 'Good job, you guessed the code'
+        return
+      else
+        puts "Numbers with correct value and position: #{result[0]}"
+        puts "Numbers with correct value, but wrong position: #{result[1]}"
+      end
+    end
+    puts 'You guessed wrong 12 times, good luck on your next try!'
   end
 end
-
