7c507f59ebfb2e2a724a9f24f8b1f0a30d76b771
Robert4361 <robert4361@gmail.com>
2022-12-10 11:04:47 +0100

    Add code instance to game class

--- game.rb
+++ game.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 require './rules'
+require './code'
 # main class for playing the game
 class Game
+  attr_reader :code
+
   def initialize
     Rules.show
+    @code = Code.new
   end
 end
+
